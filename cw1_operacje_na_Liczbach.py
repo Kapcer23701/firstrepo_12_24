dcb7219656209cfb8c626637e84c7237ca00af2c
Kacper <ptx53497@student.torun.merito.pl>
2024-12-07 13:46:45 +0100

    dodanie pierwszego cwiczenia

---
+++ cw1_operacje_na_Liczbach.py
@@ -0,0 +1,15 @@
+#dane od użytkownika
+num1_str = input("Podaj pierwszą liczbę: ")
+num2_str = input("Podaj drugą liczbę: ")
+
+num1_int= int(num1_str)
+num2_int= int(num2_str)
+
+
+# operacje matematyczne
+
+result_add = num1_int + num2_int
+potengowanie = num1_int ** num2_int
+
+# Wyświetlanie wynikow
+print(f"Wynik dodawania to: {result_add} natomiast wynik potęgowania wynosi: {potengowanie}")
