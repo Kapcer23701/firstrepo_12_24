d7c4575a4303f83ee18ca12440d41f9acb57bbd8
Kacper <ptx53497@student.torun.merito.pl>
2024-12-07 14:16:23 +0100

    Dodanie drugiego cwiczenia

---
+++ c2_lista.py
@@ -0,0 +1,12 @@
+#lista owoców
+owoce = ["banan","mandarynka","jabłko"]
+
+#słownik z cenami
+
+ceny = {"banan": 4, "mandarynka": 3, "jabłko": 1}
+
+#wyświetlanie
+
+ulubiony = owoce[0]
+
+print(f"Moj ulubiony owoc to {ulubiony} i jego cena to {ceny[ulubiony]} zł")
